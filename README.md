43ff2eaf44a89700f1d4b3113d286fbaa71f5c2a
Anna <anna.loosla@gmail.com>
2023-01-03 17:39:42 -0500

    Go uses vue, update readme.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# go
+### To rebuild vue files
+
+```bash
+cd pkg/http/web/app
+yarn run build
+```
+
+Files will be updated in /pkg/http/web/app/dist
+
+### To run Vue app only (without Go)
+port 3000
+```bash
+yarn serve
+```
+### To run Go app
+port 8000
+
+```bash
+cd pkg/http/web/app
+yarn run build # build vue files
+go run main.go
+```
--- main.go
+++ main.go
@@ -13,6 +13,11 @@ func main() {
 		io.WriteString(w, "Hello, world!\n")
 	}
 
+	// Serve static files from the frontend/dist directory.
+	fs := http.FileServer(http.Dir("./pkg/http/web/app/dist"))
+	http.Handle("/", fs)
+	log.Println("Listing for requests with Vue UI at http://localhost:8000/")
+
 	http.HandleFunc("/hello", helloHandler)
 	log.Println("Listing for requests at http://localhost:8000/hello")
 	log.Fatal(http.ListenAndServe(":8000", nil))
